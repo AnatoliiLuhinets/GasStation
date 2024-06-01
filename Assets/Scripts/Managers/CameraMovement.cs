3584c0c39be823f060cf21d3f899f3398e8acec0
Anatolii Luhinets <naisprontc@gmail.com>
2024-06-02 01:25:19 +0300

    Fixed Camera movement

--- Assets/Scripts/Managers/CameraMovement.cs
+++ Assets/Scripts/Managers/CameraMovement.cs
@@ -6,8 +6,8 @@ namespace Managers
     public class CameraMovement : MonoBehaviour
     {
         [SerializeField] private float _panSpeed = 0.5f;
-        
-        private Vector3 _lastMousePosition;
+
+        private Vector3 _lastInputPosition;
         private Vector2 _initialAngle;
         private float _currentYRotation;
         private float _maxRotationAngle = 15f;
@@ -23,9 +23,15 @@ namespace Managers
             if (IsPointerOverUIElement())
                 return;
 
+            Vector3 inputPosition = GetInputPosition();
+
+#if UNITY_EDITOR
             if (Input.GetMouseButton(0))
+#else
+            if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+#endif
             {
-                Vector3 deltaPosition = Input.mousePosition - _lastMousePosition;
+                Vector3 deltaPosition = inputPosition - _lastInputPosition;
                 float yRotationDelta = deltaPosition.x * _panSpeed * Time.deltaTime;
                 _currentYRotation += yRotationDelta;
 
@@ -33,21 +39,35 @@ namespace Managers
 
                 transform.rotation = Quaternion.Euler(_initialAngle.x, _initialAngle.y + _currentYRotation, 0);
             }
-            _lastMousePosition = Input.mousePosition;
+
+            _lastInputPosition = inputPosition;
+        }
+
+        private Vector3 GetInputPosition()
+        {
+#if UNITY_EDITOR
+            return Input.mousePosition;
+#else
+            if (Input.touchCount > 0)
+            {
+                return Input.GetTouch(0).position;
+            }
+            return new Vector3();
+#endif
         }
 
         private bool IsPointerOverUIElement()
         {
             if (EventSystem.current.IsPointerOverGameObject())
                 return true;
-            
+
+#if !UNITY_EDITOR
             for (int i = 0; i < Input.touchCount; i++)
             {
-                Touch touch = Input.GetTouch(i);
-                if (EventSystem.current.IsPointerOverGameObject(touch.fingerId))
+                if (EventSystem.current.IsPointerOverGameObject(Input.GetTouch(i).fingerId))
                     return true;
             }
-
+#endif
             return false;
         }
     }
